28370712574df6ef019300bd823a183c7eb83e20
DheepthaRai <51468745+DheepthaRai@users.noreply.github.com>
2020-10-18 15:08:38 +0530

    Add files via upload

---
+++ morse_code_converter.py
@@ -0,0 +1,32 @@
+MORSE_CODE_DICT = {'A':'.-', 'B':'-...',
+                    'C':'-.-.', 'D':'-..', 'E':'.',
+                    'F':'..-.', 'G':'--.', 'H':'....',
+                    'I':'..', 'J':'.---', 'K':'-.-',
+                    'L':'.-..', 'M':'--', 'N':'-.',
+                    'O':'---', 'P':'.--.', 'Q':'--.-',
+                    'R':'.-.', 'S':'...', 'T':'-',
+                    'U':'..-', 'V':'...-', 'W':'.--',
+                    'X':'-..-', 'Y':'-.--', 'Z':'--..',
+                    '1':'.----', '2':'..---', '3':'...--',
+                    '4':'....-', '5':'.....', '6':'-....',
+                    '7':'--...', '8':'---..', '9':'----.',
+                    '0':'-----', ', ':'--..--', '.':'.-.-.-',
+                    '?':'..--..', '/':'-..-.', '-':'-....-',
+                    '(':'-.--.', ')':'-.--.-'}
+
+def encrypt(message):
+    code = ' '
+    for letter in message:
+        if letter != ' ':
+            code += MORSE_CODE_DICT[letter] + ' '
+        else:
+            letter += ' '
+    return code
+
+def main():
+    message = input('Enter the message you want to be encrypted to morse: ')
+    result = encrypt(message.upper())
+    print(result)
+
+if __name__ == '__main__':
+    main()
